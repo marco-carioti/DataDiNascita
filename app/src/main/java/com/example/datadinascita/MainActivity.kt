036cf6c7e7d299e3dde716da038b2bf88c0956cf
marco_carioti <marcocarioti.mc@gmail.com>
2021-10-07 16:31:54 +0200

    Data di nascita

--- app/src/main/java/com/example/datadinascita/MainActivity.kt
+++ app/src/main/java/com/example/datadinascita/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.datadinascita
 
+import android.app.ProgressDialog.show
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+import com.example.datadinascita.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        onButtonClick()
+    }
+
+    private fun onButtonClick() {
+        binding.button.setOnClickListener() {
+            var data = binding.editText.text.toString().toInt()
+            var eta = 2021 - data
+        Toast.makeText(this,"$eta",Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
+
+
